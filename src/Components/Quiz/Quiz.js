9a89ff3212e52b2c6ee1822a826bd8f40b5c8f4c
MohammadNasir520 <nasirahsan520@gmail.com>
2022-10-11 03:55:52 +0600

    question background color added

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-
+import './Quiz.css'
 const Quiz = () => {
     const quizDetails=useLoaderData();
     console.log(quizDetails.data)
@@ -9,23 +9,35 @@ const Quiz = () => {
     //  const questionss =quizDetails.data.questions.map(question=> question);
     //  const question=questionss.find(q=> q.id===questionss.id)
     //  console.log(question);
+
     return (
         <div>
           <h2>  Take a Quize about : <span className='text-success'>{name}</span> </h2>
           <div>
             {
                 quizDetails.data.questions.map(question=> 
-                <div className='border border-primary'
-                key={question.id}>
-                    
-                    <p>{question.question}</p>
+                <div className='question-container border border-primary m-5 py-2 text-light '
 
+                     key={question.id}>
+
+                    {/* question */}
+                    <h4>{question.question}</h4>
+
+
+                    {/* options */}
                     <div>
                         {
-                            question.options.map(option=> <p>"{option}</p>)
+                            question.options.map(option=>
+                                 <div className=' mx-5'>
+
+                                    <p>{option}</p>
+                                    
+                                    </div>)
                         }
                     </div>
                 
+
+
                 
                 </div>)
             }
