350ea80ad2182853e45aa0cfdd61064c103dccbd
MohammadNasir520 <nasirahsan520@gmail.com>
2022-10-12 01:14:15 +0600

    responsive navbar added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,17 +5,24 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav className="nav-bar d-flex justify-content-between align-items-center  p-4 ">
-     <div>
-<h1 className="text-success">Poralekha</h1>
-     </div>
-     <div className="">
-     <NavLink to={'/home'}>Home</NavLink>
-      <NavLink to={'statistics'}>Statistics</NavLink>
-      <NavLink to={'/blogs'}>Blogs</NavLink>
-     </div>
-      {/* <NavLink to={}></NavLink> */}
-    </nav>
+    
+    <nav className="navbar navbar-expand-lg bg mt-20px">
+    <div className="container-fluid">
+      <NavLink className="navbar-brand text-success fw-bold" to="/home">Pohalekha</NavLink>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav mx-auto ">
+          <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
+          <NavLink className="nav-link" to="/statistics">Statistics</NavLink>
+          <NavLink className="nav-link" to="/blogs">Blogs</NavLink>
+          <NavLink className="nav-link  " to="/Quiz">Quiz</NavLink>
+        </div>
+      </div>
+    </div>
+  </nav>
+
   );
 };
 
