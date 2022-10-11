7dd24c377b795689233ff1cb704c4d3a5dc8ebf5
MohammadNasir520 <nasirahsan520@gmail.com>
2022-10-12 05:07:09 +0600

    rechats responsiv

--- src/Components/Statistic/Static.js
+++ src/Components/Statistic/Static.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer,  } from 'recharts';
 import'./Statistic.css'
 
 const Static = () => {
@@ -9,13 +9,14 @@ const Static = () => {
     console.log(data)
     return (
      
-          
+          <ResponsiveContainer width="80%" height="100%" >
             <BarChart   width={400} height={400} data={data}>
             <Bar dataKey={"total"} fill="#8884d8" ></Bar>
             <XAxis></XAxis>
             <YAxis></YAxis>
             <Tooltip></Tooltip>
             </BarChart>
+            </ResponsiveContainer>
            
         
     );
